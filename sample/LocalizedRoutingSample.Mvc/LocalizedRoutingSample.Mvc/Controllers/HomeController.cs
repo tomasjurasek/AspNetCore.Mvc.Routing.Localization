bd32a93ccc251633f9affdaea7854f2951031464
Tomas Jurasek <jurasek.tomas@hotmail.cz>
2021-03-28 16:39:19 +0200

    sample language

--- sample/LocalizedRoutingSample.Mvc/LocalizedRoutingSample.Mvc/Controllers/HomeController.cs
+++ sample/LocalizedRoutingSample.Mvc/LocalizedRoutingSample.Mvc/Controllers/HomeController.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using LocalizedRoutingSample.Mvc.Models;
 using AspNetCore.Mvc.Routing.Localization.Attributes;
-using AspNetCore.Mvc.Routing.Localization;
-using System.Threading.Tasks;
 
 namespace LocalizedRoutingSample.Mvc.Controllers
 {
@@ -11,13 +9,6 @@ namespace LocalizedRoutingSample.Mvc.Controllers
     [LocalizedRoute("cs-CZ", "Domu")]
     public class HomeController : Controller
     {
-        private readonly ILocalizedRoutingProvider _localizedRoutingProvider;
-
-        public HomeController(ILocalizedRoutingProvider localizedRoutingProvider)
-        {
-            _localizedRoutingProvider = localizedRoutingProvider;
-        }
-
         [LocalizedRoute("en-US", "Index")]
         [LocalizedRoute("cs-CZ", "Uvod")]
         public IActionResult Index()
@@ -35,17 +26,6 @@ namespace LocalizedRoutingSample.Mvc.Controllers
         public IActionResult Test()
         {
             return View();
-        }
-
-        public async Task<IActionResult> Language(string language)
-        {
-            var routeInformation = await _localizedRoutingProvider.ProvideRouteAsync(language, "Home", "Index", LocalizationDirection.OriginalToTranslated);
-            return RedirectToRoute(new
-            {
-                controller = routeInformation.Controller,
-                action = routeInformation.Action,
-                culture = language
-            });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ sample/LocalizedRoutingSample.Mvc/LocalizedRoutingSample.Mvc/Controllers/LanguagesController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCore.Mvc.Routing.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LocalizedRoutingSample.Mvc.Controllers
+{
+    public class LanguagesController : Controller
+    {
+        private readonly ILocalizedRoutingProvider _localizedRoutingProvider;
+
+        public LanguagesController(ILocalizedRoutingProvider localizedRoutingProvider)
+        {
+            _localizedRoutingProvider = localizedRoutingProvider;
+        }
+
+        public async Task<IActionResult> ChangeLanguage(string language)
+        {
+            var routeInformation = await _localizedRoutingProvider.ProvideRouteAsync(language, "Home", "Index", LocalizationDirection.OriginalToTranslated);
+            return RedirectToRoute(new
+            {
+                controller = routeInformation.Controller,
+                action = routeInformation.Action,
+                culture = language
+            });
+        }
+    }
+}
